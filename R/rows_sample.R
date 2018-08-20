2bd454c283cd5572d3bd7fa3ebed3e2602916d9f
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-20 11:14:59 -0400

    renamed and changes from lintr

--- R/rows_sample.R
+++ R/rows_sample.R
@@ -1,36 +1,30 @@
 #' Sample Random Rows from Data Frame
-#' 
+#'
 #' It is common to want a random sampling of data from a large data set for
-#'   analysis of various forms. The \code{rows.sample} function takes
+#'   analysis of various forms. The \code{rows_sample} function takes
 #'   arguments needed to return a random sample of a data frame. Providing the
 #'   source data frame, a sample size, and whether or not replacement is
 #'   allowed in the sampling is all that is required for returning the sample.
 #'   Row names (number typically) are returned in the sample making it easy to
 #'   identify which observations are selected into the sample.
-#' 
 #' @param df source data frame to be sampled from
 #' @param n sample size
 #' @param replace shoould the smapling method use repalcement? \code{TRUE} if
 #'   yes, \code{FALSE} (by default) if no
-#'   
 #' @keywords sample random row
-#' 
+#' @examples
+#' iris_samp <- rows_sample(iris, 5, replace = FALSE)
 #' @export
-#' 
-#' @examples 
-#' iris_samp <- rows.sample(iris, 5, replace = FALSE)
-
-rows.sample <- function(df, n, replace = FALSE) {
-  
+rows_sample <- function(df, n, replace = FALSE) {
   if (replace == FALSE && n > nrow(df)) {
-    
-    message("The value for argument 'n' exceeds the number of rows in 'df'.\n")
-    message("Please adjust 'n' to a value less than or equal to the number of 
-            rows in 'df'. \n")
+    message(
+      paste0(
+        "The value for argument 'n' exceeds the number of rows in 'df'.\n"
+        , "Please adjust 'n' to a value less than or equal to the number of"
+        , " rows in 'df'. \n"
+        )
+      )
     stop()
-      
   }
-  
   return(data.frame(df[sample(x = nrow(df), size = n, replace = replace), ]))
-  
 }
