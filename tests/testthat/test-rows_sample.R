39003f0baa7ae7866bdbc714f3888fa12a072c4f
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-08-20 14:08:44 -0400

    tests for rows_sample

---
+++ tests/testthat/test-rows_sample.R
@@ -0,0 +1,13 @@
+context("test-rows_sample")
+library(caseling)
+
+df <- data.frame("X" = rnorm(100, 0, 1), "Y" = runif(100, 0, 1))
+
+test_that("errors when sample size exceeds population, no replacement", {
+  expect_error(rows_sample(df = df, n = 200, replace = F))
+})
+
+test_that("produces sample of correct size", {
+  expect_equal(nrow(rows_sample(df, 2, replace = T)), 2)
+  expect_equal(ncol(rows_sample(df, 2, replace = T)), 2)
+})
