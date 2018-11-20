4242d26414c0af02be2fa78c0ba0e3aba6edcaab
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-20 13:38:52 -0500

    rand_num() now produces a vector of random numbers

--- R/rand_num.R
+++ R/rand_num.R
@@ -1,7 +1,9 @@
 #' Random Number Generator
 #'
-#' \code{randnum} draws a random number from a range either as an integer or a
+#' \code{rand_num} draws a random number(s) from a range either as an integer or a
 #'   float with a specified number of decimal digits
+#' @param n a numeric argument, specifies the number of random numbers the user
+#'   would like returned
 #' @param min a numeric argument, specifies the minimum number in the range to
 #'   draw from
 #' @param max a numeric argument, specifies the maximum number in the range to
@@ -12,13 +14,13 @@
 #' @param digits \code{optional} a number value, to accompany
 #'   \code{type = "float"} and specify the number of decimal digits to include
 #' @examples
-#' randnum(min = 1, max = 10, type = "integer")
+#' rand_num(min = 1, max = 10, type = "integer")
 #'
-#' randnum(min = 1, max = 10, type = "float", digits = 3)
+#' rand_num(min = 1, max = 10, type = "float", digits = 3)
 #'
-#' randnum(min = 1, max = 10, type = "float", digits = "5")
+#' rand_num(min = 1, max = 10, type = "float", digits = "5")
 #' @export
-randnum <- function(min = 1, max = 1000, type = "integer", digits = NA) {
+rand_num <- function(n = NULL, min = NULL, max = NULL, type = NULL, digits = NULL) {
   # Min should be less than or equal to max, if not switch values
   if (min > max) {
     message(
@@ -35,20 +37,18 @@ randnum <- function(min = 1, max = 1000, type = "integer", digits = NA) {
   # Error handling for number of digits
   # Should be a number value (either as a numeric or character class)
   # Should be an integer
-  if (type == "float" & is.na(digits)) {
-    message(
+  if (type == "float" & is.null(digits)) {
+    stop(
       "Error: Argument 'digits' must have a number value for type = 'float'.\n"
       )
-    stop()
   } else if (type == "float" & class(digits) != "numeric") {
     if (is.na(as.numeric(digits))) {
-      message(
+      stop(
         paste0(
           "Error: Argument 'digits' must be a numeric or character string of a"
           , " number value.\nE.g., 1 or '1', but not 'e'.\n"
           )
         )
-      stop()
     } else {
       message(
         paste0(
@@ -59,17 +59,16 @@ randnum <- function(min = 1, max = 1000, type = "integer", digits = NA) {
       digits <- round(as.numeric(digits), 0)
     }
   } else if (type == "float") {
-    return(round(runif(1, min = min, max = max), digits = digits))
+    return(round(runif(n = n, min = min, max = max), digits = digits))
   } else if (type == "integer") {
     digits <- 0
-    return(round(runif(1, min = min, max = max), digits = digits))
+    return(round(runif(n = n, min = min, max = max), digits = digits))
   } else {
-    message(
+    stop(
       paste0(
         "Error: Argument 'type' must have a value of either 'integer' or"
         , "'float'.\n"
         )
     )
-    stop()
   }
 }
