06b30cd1f3adc9951d6ccd539d564cd1fbb2b603
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-11-20 14:00:54 -0500

    update no_subs argument to num_subs

--- tests/testthat/test-sub_frame.R
+++ tests/testthat/test-sub_frame.R
@@ -4,5 +4,5 @@ library(caseling)
 df <- data.frame("X" = rnorm(100, 0, 1), "Y" = runif(100, 0, 1))
 
 test_that("no_subs can be NULL", {
-  expect_silent(sub_frame(df = df, no_subs = NULL))
+  expect_silent(sub_frame(df = df, num_subs = NULL))
 })
