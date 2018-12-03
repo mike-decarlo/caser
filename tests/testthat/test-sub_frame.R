52fff8542aac0faafc4e41b271e31a336a45b0d4
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-03 07:30:34 -0500

    new test for warnings of non-df conversion to df

--- tests/testthat/test-sub_frame.R
+++ tests/testthat/test-sub_frame.R
@@ -6,3 +6,8 @@ df <- data.frame("X" = rnorm(100, 0, 1), "Y" = runif(100, 0, 1))
 test_that("num_subs can be NULL", {
   expect_silent(sub_frame(df = df, num_subs = NULL))
 })
+
+test_that("if df not data.frame it gets converted", {
+  df <- as.matrix(df)
+  expect_warning(sub_frame(df = df, num_subs = 4))
+})
