5512fcd8e5fdbbdc914d4c7598231ccc1d9e2030
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-03 07:32:29 -0500

    df argument as a null should error

--- tests/testthat/test-sub_frame.R
+++ tests/testthat/test-sub_frame.R
@@ -11,3 +11,7 @@ test_that("if df not data.frame it gets converted", {
   df <- as.matrix(df)
   expect_warning(sub_frame(df = df, num_subs = 4))
 })
+
+test_that("null df values error", {
+  expect_error(sub_frame(df = NULL, num_subs = 4))
+})
