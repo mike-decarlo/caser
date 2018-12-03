041aa3955173143b0b3c608bdcd748b57b44b41a
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-03 07:08:05 -0500

    updated comment of test for sub_frame

--- tests/testthat/test-sub_frame.R
+++ tests/testthat/test-sub_frame.R
@@ -3,6 +3,6 @@ library(caseling)
 
 df <- data.frame("X" = rnorm(100, 0, 1), "Y" = runif(100, 0, 1))
 
-test_that("no_subs can be NULL", {
+test_that("num_subs can be NULL", {
   expect_silent(sub_frame(df = df, num_subs = NULL))
 })
