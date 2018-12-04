181b6fe645a898938f2b67a8e51f563b2c18a5e7
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-04 11:45:02 -0500

    updates to testing for verbose argument

--- tests/testthat/test-sort_columns.R
+++ tests/testthat/test-sort_columns.R
@@ -3,9 +3,15 @@ context("test-sort_columns")
 test_that("sorting cols works when all inputs are entered correctly", {
   x <- data.frame(b = c(6, 7, 8, 9, 10), a = c(1, 2, 3, 4, 5))
   expect_message(
-    sort_columns(x = x, order = "asc")
+    sort_columns(x = x, order = "asc", verbose = T)
   )
   expect_message(
+    sort_columns(x = x, order = "desc", verbose = T)
+  )
+  expect_silent(
+    sort_columns(x = x, order = "asc")
+  )
+  expect_silent(
     sort_columns(x = x, order = "desc")
   )
 })
