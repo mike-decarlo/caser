3c0b8d2f7bb196867342f493ef5e877a9986bcd3
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-04 12:18:59 -0500

    added testing for import_batch_files

---
+++ tests/testthat/test-import_batch_files.R
@@ -0,0 +1,22 @@
+context("test-import_batch_files")
+d <- data.frame("a" = seq(1, 10), "b" = seq(2, 20, by = 2))
+write.csv(d, "./dat.csv")
+test_that("NULL values for 'p'/'f' return errors", {
+  expect_error(
+    import_batch_files(f = read.csv)
+  )
+  expect_error(
+    import_batch_files(p = ".")
+  )
+})
+test_that("Non existent directory for 'p' returns error", {
+  expect_error(
+    import_batch_files(p = "./data", f = read.csv, ext = ".csv")
+  )
+})
+test_that("Silent when working correctly", {
+  expect_silent(
+    import_batch_files(p = ".", f = read.csv, ext = ".csv")
+  )
+})
+file.remove("./dat.csv")
