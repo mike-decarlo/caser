bd1e867c2865b050ae2d16de658b3771104ed7e4
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-04 10:16:52 -0500

    Renamed function to verb

--- tests/testthat/test-generate_random_number.R
+++ tests/testthat/test-generate_random_number.R
@@ -1,33 +1,33 @@
-context("test-rand_num")
+context("test-generate_random_number")
 library(caser)
 
 test_that("as integer and as numeric are equivalent", {
-  random <- rand_num(1, 1, 2, "integer")
+  random <- generate_random_number(1, 1, 2, "integer")
   expect_equal(as.integer(random), random)
 })
 
 test_that("integer with digits is same as without", {
-  random1 <- rand_num(1, 1, 1, "integer")
-  random2 <- rand_num(1, 1, 1, "integer", digits = 2)
+  random1 <- generate_random_number(1, 1, 1, "integer")
+  random2 <- generate_random_number(1, 1, 1, "integer", digits = 2)
   expect_equal(random1, random2)
 })
 
 test_that("float number characters is correct", {
-  random <- rand_num(1, 1.1, 1.9, "float", 0)
+  random <- generate_random_number(1, 1.1, 1.9, "float", 0)
   expect_equal(nchar(random), 1)
-  random <- rand_num(1, 1.1, 1.9, "float", 1)
+  random <- generate_random_number(1, 1.1, 1.9, "float", 1)
   expect_gte(nchar(random), 3)
-  random <- rand_num(1, 1.1, 1.9, "float", 2)
+  random <- generate_random_number(1, 1.1, 1.9, "float", 2)
   expect_gte(nchar(random), 3)
-  random <- rand_num(1, 1.1, 1.9, "float", 3)
+  random <- generate_random_number(1, 1.1, 1.9, "float", 3)
   expect_gte(nchar(random), 3)
 })
 
 test_that("exits on NA error or non-number for float", {
-  expect_error(rand_num(1, 1, 2, "float"))
-  expect_error(rand_num(1, 1, 2, "float", "A"))
+  expect_error(generate_random_number(1, 1, 2, "float"))
+  expect_error(generate_random_number(1, 1, 2, "float", "A"))
   expect_message(
-    rand_num(1, 1, 2, "float", "3")
+    generate_random_number(1, 1, 2, "float", "3")
     , paste0(
       "Non-numeric value entered for argument 'digits'.\n"
       , "Converting to numeric...\n"
@@ -37,7 +37,7 @@ test_that("exits on NA error or non-number for float", {
 
 test_that("max gte min", {
   expect_message(
-    rand_num(1, 2, 1, "integer")
+    generate_random_number(1, 2, 1, "integer")
     , paste0(
         "Warning: Min must be less than or equal to max.\n"
         , "Switching values to satisfy requirements.\n"
@@ -46,5 +46,5 @@ test_that("max gte min", {
 })
 
 test_that("type argument is either 'integer' or 'float'", {
-  expect_error(rand_num(1, 1, 2, type = "other"))
+  expect_error(generate_random_number(1, 1, 2, type = "other"))
 })
