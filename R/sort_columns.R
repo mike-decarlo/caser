0feb0880152eb10b08eec5a953e11f52c719fa7d
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-12-04 11:35:17 -0500

    option to print messages or not using verbose argument

--- R/sort_columns.R
+++ R/sort_columns.R
@@ -2,11 +2,14 @@
 #' \code{sort_columns} takes two arguments, the name of a dataframe and the order
 #'   (ascending or descending), and will then provide the dataframe in the
 #'   column sorting as specified.
-#' @param x a dataframe object.
-#' @param order asc or desc for ascending or descending ordering respectively.
-#' @keywords sort columns dataframe
+#' @param x dataframe object
+#' @param order character string; if \code{"asc"}, the function sorts in
+#'   ascending order; if \code{"desc"}, the function sorts in descending order.
+#' @param verbose logical; if \code{TRUE}, the function prints messages about
+#'   objects being sorted.
+#' @keywords sort columns
 #' @export
-sort_columns <- function(x = NULL, order = "asc") {
+sort_columns <- function(x = NULL, order = "asc", verbose = FALSE) {
   if (is.null(x)) {
     stop(
       "The 'x' argument must be a non-null, dataframe object.\n"
@@ -25,17 +28,19 @@ sort_columns <- function(x = NULL, order = "asc") {
       )
     )
   }
-  message(
-    paste0(
-      "\nObject 'x' columns being sorted in "
-      , ifelse(order == "asc", "ascending", "descending")
-      , ", alphabetical order.\n"
+  if (verbose == TRUE) {
+    message(
+      paste0(
+        "\nObject 'x' columns being sorted in "
+        , ifelse(order == "asc", "ascending", "descending")
+        , ", alphabetical order.\n"
+      )
     )
-  )
+  }
   if (order == "asc") {
     x <- x[, order(colnames(x), decreasing = F)]
   } else if (order == "desc") {
     x <- x[, order(colnames(x), decreasing = T)]
   }
-  return(x)
+  x
 }
