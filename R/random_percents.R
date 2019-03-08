c3742c0bc39543ed90d84f605643cb2292eb9174
Mike <8523449+wolfm4ne@users.noreply.github.com>
2019-03-08 11:34:07 -0500

    introduction of random_percents function

---
+++ R/random_percents.R
@@ -0,0 +1,28 @@
+#' Return a vector of random proportions/percents that sum to 1/100%
+#'
+#' Useful to split a data value into random parts that will sum back to the
+#'   total.
+#' @param b numeric; the number of bins to generate.
+#' @param s numeric; the seed for randomly generating numbers.
+#' @keywords random percent proportion
+#' @examples
+#' # With default seed value (1)
+#' random_percents(b = 2)
+#' # With specific seed value (45)
+#' random_percents(b = 2, s = 45)
+#' # With specific seed value (45) and more bins (9)
+#' random_percents(b = 9, s = 45)
+#' @export
+random_percents <- function(b = NULL, s = 1) {
+  if (is.numeric(s)) {
+    set.seed(s)
+  } else {
+    stop("\n's' must be a numeric value.\n")
+  }
+  if (!is.numeric(b) | b == 0) {
+    stop("\n'b' must be a numeric value > 0.\n")
+  } else {
+    x <- round(runif(b, 0, 100), 0)
+    (x / sum(x))
+  }
+}
---
+++ tests/testthat/test-random_percents.R
@@ -0,0 +1,33 @@
+context("test-random_percents")
+library(caser)
+
+test_that("errors when non-numeric 'b'", {
+  expect_error(
+    random_percents(b = NULL)
+  )
+  expect_error(
+    random_percents(b = "b")
+  )
+})
+
+test_that("errors when non-numeric 's'", {
+  expect_error(
+    random_percents(b = 2, s = NULL)
+  )
+  expect_error(
+    random_percents(b = 2, s = "s")
+  )
+})
+
+test_that("errors when 0 for 'b'", {
+  expect_error(
+    random_percents(b = 0)
+  )
+})
+
+test_that("produces output when acceptable values for 'b' and 's'", {
+  expect_equal(
+    random_percents(b = 2, s = 1)
+    , random_percents(b = 2, s = 1)
+  )
+})
