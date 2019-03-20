32346911b32643395a4ba4ea76659d67f547d0fd
Mike <8523449+wolfm4ne@users.noreply.github.com>
2019-03-20 12:30:05 -0400

    consistency across package in use of df instead of x for data frame arguments and verbage for documentation

--- R/sample_rows.R
+++ R/sample_rows.R
@@ -7,15 +7,19 @@
 #'   allowed in the sampling is all that is required for returning the sample.
 #'   Row names (number typically) are returned in the sample making it easy to
 #'   identify which observations are selected into the sample.
-#' @param df source data frame to be sampled from
-#' @param n sample size
-#' @param replace shoould the smapling method use repalcement? \code{TRUE} if
+#' @param df data.frame; the source \bold{data.frame} to be sampled from
+#' @param n numeric; sample size
+#' @param s \code{(optional)} numeric; the \bold{seed} for randomly generating numbers.
+#' @param replace logical; should the sampling method use repalcement? \code{TRUE} if
 #'   yes, \code{FALSE} (by default) if no
 #' @keywords sample random row
 #' @examples
 #' iris_samp <- sample_rows(iris, 5, replace = FALSE)
 #' @export
-sample_rows <- function(df, n, replace = FALSE) {
+sample_rows <- function(df = NULL, n, s = NULL, replace = FALSE) {
+  if (is.null(df) || class(df) != "data.frame") {
+    stop("\nThe argument 'df' must be a non-NULL data.frame.\n")
+  }
   if (replace == FALSE && n > nrow(df)) {
     message(
       paste0(
@@ -25,6 +29,9 @@ sample_rows <- function(df, n, replace = FALSE) {
         )
       )
     stop()
+  }
+  if(!is.null(s)){
+    set.seed(s)
   }
   d <- as.data.frame(df[sample(x = nrow(df), size = n, replace = replace), ])
   colnames(d) <- colnames(df)
