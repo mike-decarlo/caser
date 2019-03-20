751069ad4d4a568407b6bacff01e33d5a8b1f9c5
Mike <8523449+wolfm4ne@users.noreply.github.com>
2019-03-20 12:30:24 -0400

    consistency across package in use of df instead of x for data frame arguments and verbage for documentation

--- R/sort_columns.R
+++ R/sort_columns.R
@@ -2,17 +2,17 @@
 #' \code{sort_columns} takes two arguments, the name of a dataframe and the
 #'   order (ascending or descending), and will then provide the dataframe in
 #'   the column sorting as specified.
-#' @param x dataframe object
-#' @param order character string; if \code{"asc"}, the function sorts in
+#' @param df data.frame; the source \bold{data.frame} to be sorted.
+#' @param order string; if \code{"asc"}, the function sorts in
 #'   ascending order; if \code{"desc"}, the function sorts in descending order.
 #' @param verbose logical; if \code{TRUE}, the function prints messages about
 #'   objects being sorted.
 #' @keywords sort columns
 #' @export
-sort_columns <- function(x = NULL, order = "asc", verbose = FALSE) {
-  if (is.null(x)) {
+sort_columns <- function(df = NULL, order = "asc", verbose = FALSE) {
+  if (is.null(df)) {
     stop(
-      "The 'x' argument must be a non-null, dataframe object.\n"
+      "The 'df' argument must be a non-null, dataframe object.\n"
     )
   }
   if (class(order) != "character") {
@@ -31,16 +31,16 @@ sort_columns <- function(x = NULL, order = "asc", verbose = FALSE) {
   if (verbose == TRUE) {
     message(
       paste0(
-        "\nObject 'x' columns being sorted in "
+        "\nObject 'df' columns being sorted in "
         , ifelse(order == "asc", "ascending", "descending")
         , ", alphabetical order.\n"
       )
     )
   }
   if (order == "asc") {
-    x <- x[, order(colnames(x), decreasing = F)]
+    df <- df[, order(colnames(df), decreasing = F)]
   } else if (order == "desc") {
-    x <- x[, order(colnames(x), decreasing = T)]
+    df <- df[, order(colnames(df), decreasing = T)]
   }
-  x
+  df
 }
--- tests/testthat/test-sort_columns.R
+++ tests/testthat/test-sort_columns.R
@@ -3,20 +3,20 @@ context("test-sort_columns")
 test_that("sorting cols works when all inputs are entered correctly", {
   x <- data.frame(b = c(6, 7, 8, 9, 10), a = c(1, 2, 3, 4, 5))
   expect_message(
-    sort_columns(x = x, order = "asc", verbose = T)
+    sort_columns(df = x, order = "asc", verbose = T)
   )
   expect_message(
-    sort_columns(x = x, order = "desc", verbose = T)
+    sort_columns(df = x, order = "desc", verbose = T)
   )
   expect_silent(
-    sort_columns(x = x, order = "asc")
+    sort_columns(df = x, order = "asc")
   )
   expect_silent(
-    sort_columns(x = x, order = "desc")
+    sort_columns(df = x, order = "desc")
   )
 })
 
-test_that("errors when x null", {
+test_that("errors when df null", {
   expect_error(
     sort_columns(order = "asc")
   )
@@ -28,13 +28,13 @@ test_that("errors when x null", {
 test_that("errors if not 'asc' or 'desc' entered for order", {
   x <- data.frame(b = c(6, 7, 8, 9, 10), a = c(1, 2, 3, 4, 5))
   expect_error(
-    sort_columns(x = x, order = "error")
+    sort_columns(df = x, order = "error")
   )
 })
 
 test_that("errors if order not a character input", {
   x <- data.frame(b = c(6, 7, 8, 9, 10), a = c(1, 2, 3, 4, 5))
   expect_error(
-    sort_columns(x = x, order = 0)
+    sort_columns(df = x, order = 0)
   )
 })
