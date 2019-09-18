a3bd3571aac70c6c7aafe90d78a7501060594767
Mike <8523449+wolfm4ne@users.noreply.github.com>
2019-09-18 13:52:17 -0400

    addition of new function, sample_compliment()

---
+++ R/sample_compliment.R
@@ -0,0 +1,18 @@
+#' @title A Sampling Compliment Function
+#' 
+#' @description Designed to work in tandem with \code{sample_rows} to return
+#'   a complimentary data set to a sample from an original set.
+#' @param orig a data.frame; the original/complete data set being sampled
+#' @param samp a data.frame; a subset, randomly sampled from the \code{orig}
+#' @examples
+#' iris_test <- sample_rows(
+#'   df = iris
+#'   , n = floor(0.2 * nrow(iris))
+#'   , s = 1
+#'   , replace = F
+#' )
+#' iris_train <- sample_compliment(orig = iris, samp = iris_test)
+#' @export
+sample_compliment <- function(orig, samp) {
+  c <- as.data.frame(orig[-as.numeric(rownames(samp)), ])
+}
