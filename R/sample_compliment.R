ff228cdf2b6455832455e22537986b89f8efab9e
Mike <8523449+wolfm4ne@users.noreply.github.com>
2019-09-20 10:54:07 -0400

    changed so objects of all types can be entered; fixed broken compliment row naming

--- R/sample_compliment.R
+++ R/sample_compliment.R
@@ -2,17 +2,25 @@
 #'
 #' @description Designed to work in tandem with \code{sample_rows} to return
 #'   a complimentary data set to a sample from an original set.
-#' @param orig a data.frame; the original/complete data set being sampled
-#' @param samp a data.frame; a subset, randomly sampled from the \code{orig}
+#' @param orig an object; the original/complete data set being sampled
+#' @param samp an object; a subset, randomly sampled from the \code{orig}
 #' @examples
 #' iris_test <- sample_rows(
-#'   df = iris
+#'   x = iris
 #'   , n = floor(0.2 * nrow(iris))
-#'   , s = 1
+#'   , seed = 1
 #'   , replace = FALSE
 #' )
 #' iris_train <- sample_compliment(orig = iris, samp = iris_test)
 #' @export
-sample_compliment <- function(orig, samp) {
+sample_compliment <- function(orig = NULL, samp = NULL) {
+  orig <- as.data.frame(orig)
+  samp <- as.data.frame(samp)
+  rn <- rownames(orig)
+  inc <- rownames(orig) %in% rownames(samp)
+  rn <- rn[inc == FALSE]
   c <- as.data.frame(orig[-as.numeric(rownames(samp)), ])
+  colnames(c) <- colnames(orig)
+  rownames(c) <- rn
+  c
 }
