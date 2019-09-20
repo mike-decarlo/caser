d5a241b017c566938c5af0c72d1f0f77c5719d1e
Mike <8523449+wolfm4ne@users.noreply.github.com>
2019-09-20 10:56:46 -0400

    Updated parameters

--- tests/testthat/test-sample_rows.R
+++ tests/testthat/test-sample_rows.R
@@ -1,39 +1,39 @@
 context("test-sample_rows")
 library(caser)
 
-df <- NULL
+x <- NULL
 
-test_that("error if NULL df argument or NULL data.frame object", {
+test_that("error if NULL x argument or NULL data.frame object", {
     expect_error(
-      sample_rows(df = df, n = 50)
+      sample_rows(x = x, n = 50)
     )
     expect_error(
-      sample_rows(df = NULL, n = 50)
+      sample_rows(x = NULL, n = 50)
     )
   })
 
-df <- data.frame("X" = rnorm(100, 0, 1), "Y" = runif(100, 0, 1))
+x <- data.frame("X" = rnorm(100, 0, 1), "Y" = runif(100, 0, 1))
 
-test_that("error if df is not class = data.frame", {
+test_that("error if x is not class = data.frame", {
   expect_error(
-    sample_rows(df = as.matrix(df), n = 50)
+    sample_rows(x = as.matrix(x), n = 50)
   )
 })
 
 test_that("errors when sample size exceeds population, no replacement", {
-  expect_error(sample_rows(df = df, n = 200, replace = F))
+  expect_error(sample_rows(x = x, n = 200, replace = F))
 })
 
 test_that("Seed control works", {
   expect_silent(
-    sample_rows(df = df, n = 50, s = 1, replace = F)
+    sample_rows(x = x, n = 50, seed = 1, replace = F)
   )
   expect_silent(
-    sample_rows(df = df, n = 50, replace = F)
+    sample_rows(x = x, n = 50, replace = F)
   )
 })
 
 test_that("produces sample of correct size", {
-  expect_equal(nrow(sample_rows(df = df, n = 2, replace = T)), 2)
-  expect_equal(ncol(sample_rows(df = df, n = 2, replace = T)), 2)
+  expect_equal(nrow(sample_rows(x = x, n = 2, replace = T)), 2)
+  expect_equal(ncol(sample_rows(x = x, n = 2, replace = T)), 2)
 })
