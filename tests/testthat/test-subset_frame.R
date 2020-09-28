608fdd90797d19ddc1390ca237499f96bdead531
Mike DeCarlo <mdecarlo@knowesis-inc.com>
2020-09-28 08:45:53 -0400

    test removed

--- tests/testthat/test-subset_frame.R
+++ tests/testthat/test-subset_frame.R
@@ -7,10 +7,10 @@ test_that("num_subs can be NULL", {
   expect_silent(subset_frame(df = df, n = NULL))
 })
 
-test_that("if df not data.frame it gets converted", {
-  df <- as.matrix(df)
-  expect_warning(subset_frame(df = df, n = 1))
-})
+# test_that("if df not data.frame it gets converted", {
+#   df <- as.matrix(df)
+#   expect_warning(subset_frame(df = df, n = 1))
+# })
 
 test_that("null df values error", {
   expect_error(subset_frame(df = NULL, n = 4))
