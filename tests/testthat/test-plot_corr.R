ecd3afa2252bc9465051ddbd90197ad6d94e23a3
Mike DeCarlo <decarlo.michael.j@gmail.com>
2023-03-13 08:37:33 -0400

    initial test of plotting

--- tests/testthat/test-plot_corr.R
+++ tests/testthat/test-plot_corr.R
@@ -5,3 +5,9 @@ test_that("errors if an argument is NULL", {
   expect_error(plot_corr(NULL, "test"))
   expect_error(plot_corr(data.frame("one" = 1, "two" = 2), NULL))
 })
+test_that("creates snapshot", {
+  expect_type(
+    plot_corr(data.frame("one" = runif(30), "two" = runif(30)), "one"),
+    "list"
+  )
+})
